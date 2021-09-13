a3f1ac7d739040959b9e1674d322e2846798c0be
MuliaAndrew <90112978+MuliaAndrew@users.noreply.github.com>
2021-09-13 17:15:54 +0300

    Update solve_square_eq.cpp

--- solve_square_eq.cpp
+++ solve_square_eq.cpp
@@ -2,52 +2,47 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-
-// description + installation + launch
-
-// debug + release
-
-// README.md
-
-// for * <assert.h>
-
-
+#include <assert.h>
 
 //-----------------------------------------------------------------------------
 
-
 void Square_Eq_Solutions(double coef_a, double coef_b, double coef_c, double* x1, double* x2)
 {
-    int num_roots = Num_Of_Square_Eq_Roots(coef_a, coef_b, coef_c);
+    assert(x1);
+    assert(x2);
+
+    int num_roots = Num_Square_Eq_Roots(coef_a, coef_b, coef_c);
     switch (num_roots)
     {
-        case N_Of_Square_Eq_Roots::TWO: // (Num...) TWO
+        case N_Sq_Roots::TWO:
         {
             Solve_Two_Roots_Case(coef_a, coef_b, coef_c, x1, x2);
             printf("x1 = %lg, x2 = %lg", *x1, *x2);
 
             break;
         }
-        case N_Of_Square_Eq_Roots::ONE_LINEAR:
+        case N_Sq_Roots::ONE_LINEAR:
         {
             *x1 = Solve_Line_Eq(coef_b, coef_c);
             printf("x = %lg", *x1);
 
             break;
         }
-        case N_Of_Square_Eq_Roots::ONE_SQUARE:
+        case N_Sq_Roots::ONE_SQUARE:
         {
             *x1 = -coef_b / (2 * coef_a);
-            printf("x = %lg", *x1);
-
+            if(isfinite(*x1))
+                printf("x = %lg", *x1);
+            else
+                printf("Warning: value of x1 is not finite. x1 = NAN");
             break;
         }
-        case N_Of_Square_Eq_Roots::NO:
+        case N_Sq_Roots::NO:
         {
             printf("No solutions");
             break;
         }
-        case N_Of_Square_Eq_Roots::INF:
+        case N_Sq_Roots::INF:
         {
             printf("Infinite number of solutions");
             break;
@@ -60,14 +55,16 @@ void Square_Eq_Solutions(double coef_a, double coef_b, double coef_c, double* x1
 
 void Solve_Two_Roots_Case(double coef_a, double coef_b, double coef_c, double* x1, double* x2)
 {
-    double discriminant = Solve_D(coef_a, coef_b, coef_c);      // func
+    double discriminant = Solve_Discr(coef_a, coef_b, coef_c);      // func
 
     *x1 = (-coef_b + sqrt(discriminant)) / (2 * coef_a);
     *x2 = (-coef_b - sqrt(discriminant)) / (2 * coef_a);
+    if(!(isfinite(*x1) && isfinite(*x2)))
+        printf("Warning: value of x1 and x2 is not finite. x1 = NAN, x2 = NAN");
 }
 
 
-N_Of_Square_Eq_Roots Num_Of_Square_Eq_Roots(double coef_a, double coef_b, double coef_c)
+N_Sq_Roots Num_Square_Eq_Roots(double coef_a, double coef_b, double coef_c)
 {
     if (Comparing_Doubles(coef_a, ZERO) == 0)
     {
@@ -75,55 +72,51 @@ N_Of_Square_Eq_Roots Num_Of_Square_Eq_Roots(double coef_a, double coef_b, double
         {
             if (Comparing_Doubles(coef_c, ZERO) == 0)
             {
-                return N_Of_Square_Eq_Roots::INF;
+                return N_Sq_Roots::INF;
             }
             else
             {
-                return N_Of_Square_Eq_Roots::NO;
+                return N_Sq_Roots::NO;
             }
         }
         else
         {
-            return N_Of_Square_Eq_Roots::ONE_LINEAR;
+            return N_Sq_Roots::ONE_LINEAR;
         }
     }
     else
     {
-        int discriminant = Solve_D(coef_a, coef_b, coef_c);
+        double discriminant = Solve_Discr(coef_a, coef_b, coef_c);
 
         if (Comparing_Doubles(discriminant, ZERO) == Compare_Res::LESS)
         {
-            return N_Of_Square_Eq_Roots::NO;
+            return N_Sq_Roots::NO;
         }
         else
         {
             if(Comparing_Doubles(discriminant, ZERO) == Compare_Res::EQUALS)
             {
-                return N_Of_Square_Eq_Roots::ONE_SQUARE;
+                return N_Sq_Roots::ONE_SQUARE;
             }
             else
             {
-                return N_Of_Square_Eq_Roots::TWO;
+                return N_Sq_Roots::TWO;
             }
         }
     }
 }
 
-
 //-----------------------------------------------------------------------------
 
-
-Compare_Res Comparing_Doubles(double double_arg1, double double_arg2) // names
+Compare_Res Comparing_Doubles(double num1, double num2)
 {
-     // global const
-
-    if (double_arg1 - double_arg2 > BORDER_NUMBER)
+    if (num1 - num2 > BORDER_NUMBER)
     {
         return Compare_Res::MORE;
     }
     else
     {
-        if (double_arg1 - double_arg2 < -BORDER_NUMBER)
+        if (num1 - num2 < -BORDER_NUMBER)
         {
             return Compare_Res::LESS;
         }
@@ -134,18 +127,22 @@ Compare_Res Comparing_Doubles(double double_arg1, double double_arg2) // names
     }
 }
 
-
 //-----------------------------------------------------------------------------
 
-
-double Solve_Line_Eq(double coef_a, double coef_b) // NAN   -- printf
+double Solve_Line_Eq(double coef_a, double coef_b)
 {
-    return -coef_b / coef_a; // /0 + isfinit
+    if(isfinite(coef_b / coef_a))
+        return -coef_b / coef_a;
+    else
+    {
+        printf("Error:returning value of functioin solve_square_eq.h/Solve_Line_Eq() is not finite. Returned 0\n");
+        return 0;
+    }
 }
 
 //-----------------------------------------------------------------------------
 
-double Solve_D(double coef_a, double coef_b, double coef_c)
+double Solve_Discr(double coef_a, double coef_b, double coef_c)
 {
     return coef_b * coef_b - 4 * coef_a * coef_c;
 }
